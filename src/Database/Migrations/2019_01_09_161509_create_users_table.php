a8c46339d5600e82e8e2204a296381564434c156
Kolado Sidibe <ksidibe@yahoo.com>
2019-01-09 18:26:36 -0500

    Missing from previous commit

---
+++ src/Database/Migrations/2019_01_09_161509_create_users_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('last_name')->index();
+            $table->string('middle_name')->nullable();
+            $table->string('first_name')->index();
+            $table->date('dob')->nullable();
+			$table->bigInteger('gender_id', false, true )->nullable();
+            $table->string('email')->nullable();
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->boolean('is_active')->default(true);
+            $table->string('disk')->nullable();
+            $table->string('path_to_picture')->nullable();
+            $table->string('picture_url')->nullable();
+            $table->rememberToken();
+            
+            $table->timestamps();
+            $table->softDeletes();
+
+            /**
+             * uncomment if needed
+             */
+            // $table->foreign('gender_id')->references('id')->on('genders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
