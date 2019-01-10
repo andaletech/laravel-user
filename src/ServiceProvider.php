4acc61966feabddd53e5a23e2680b19c1a26e0c7
Kolado Sidibe <ksidibe@yahoo.com>
2019-01-10 17:57:21 -0500

    Add auto discovery in composer and service provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+/**
+ * Service provider for this package.
+ *
+ * @author Kolado Sidibe <kolado.sidibe@andaletech.com>
+ * @copyright 2019 Andale Technologies, SARL
+ * @license MIT
+ */
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/aTechLaravelUser.php' => config_path('aTechLaravelUser.php')
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/config/aTechLaravelUser.php', 'aTechLaravelUser');
+    }
+}
